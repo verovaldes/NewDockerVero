15bb797bba54d169e07b886b3cce71546bbe3510
veronica valdes <verovaldes@MacBook-Air-de-veronica.local>
2020-10-12 13:11:15 -0300

    --all

--- Test_githubdocker/index.js
+++ Test_githubdocker/index.js
@@ -1,6 +1,6 @@
 var Express =require('express');
 var app=Express();
 app.get('/',function(req,res){
-    res.send('Hello World (from Docker!)');
+    res.send('Hello World (from Docker and github!)');
 });
 app.listen(8080);
